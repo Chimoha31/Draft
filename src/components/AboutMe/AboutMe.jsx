198e7d8d22ca20cf0199b3027749f1e9d70baab2
Chimoha31 <mokochii1108@gmail.com>
2022-03-21 02:50:20 -0700

    Made it for mrnu bargit add .

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,21 +1,57 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import Timeline from "./Timeline";
 import HeaderTimeline from "./HeaderTimeline";
+import "./Timeline.css";
+import "./AboutMe.css";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import { VerticalTimeline } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import MenuBar from './menu/MenuBar';
-import Timeline from './Timeline';
-import "./Timeline.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import HomeIcon from "@mui/icons-material/Home";
+import WorkIcon from "@mui/icons-material/Work";
+import ComputerIcon from "@mui/icons-material/Computer";
 
 function AboutMe() {
-  
+  const [toggle, setToggle] = useState(false);
+  const clickHandler = () => {
+    setToggle(!toggle);
+  };
   return (
     <Div>
-      <MenuBar />
-      <HeaderTimeline />
-      <VerticalTimeline>
-        <Timeline />
-      </VerticalTimeline>
+      <div className={toggle ? "page rotate" : "page"}>
+        <span className="menu-toggle" onClick={clickHandler}>
+          <MenuIcon className="menu-open" />
+          <CloseIcon className="menu-close" />
+        </span>
+        <ul className="menu-items">
+          <li>
+            <Link to="/" className="home">
+              <HomeIcon />
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="" className="works">
+              <WorkIcon />
+              Works
+            </Link>
+          </li>
+          <li>
+            <Link to="" className="skills">
+              <ComputerIcon />
+              Skills & Resume
+            </Link>
+          </li>
+        </ul>
+        <div className="timeline_container">
+          <HeaderTimeline />
+          <VerticalTimeline>
+            <Timeline />
+          </VerticalTimeline>
+        </div>
+      </div>
     </Div>
   );
 }
@@ -25,6 +61,8 @@ const Div = styled.div`
   font-family: "Montserrat", sans-serif;
   font-size: 16px;
   color: black;
+  line-height: 1.5;
+  height: 100vh;
 `;
 
 export default AboutMe;
