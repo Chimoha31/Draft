c6fece3ac1f0991c729342ba70dfd28558e7f852
Chimoha31 <mokochii1108@gmail.com>
2022-03-21 03:05:53 -0700

    Clean codes

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,50 +1,20 @@
 import React, { useState } from "react";
+import NavBar from './NavBar';
 import Timeline from "./Timeline";
 import HeaderTimeline from "./HeaderTimeline";
 import "./Timeline.css";
 import "./AboutMe.css";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { VerticalTimeline } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from "@mui/icons-material/Close";
-import HomeIcon from "@mui/icons-material/Home";
-import WorkIcon from "@mui/icons-material/Work";
-import ComputerIcon from "@mui/icons-material/Computer";
+
 
 function AboutMe() {
-  const [toggle, setToggle] = useState(false);
-  const clickHandler = () => {
-    setToggle(!toggle);
-  };
+  const [toggle, setToggle]= useState(false);
   return (
     <Div>
       <div className={toggle ? "page rotate" : "page"}>
-        <span className="menu-toggle" onClick={clickHandler}>
-          <MenuIcon className="menu-open" />
-          <CloseIcon className="menu-close" />
-        </span>
-        <ul className="menu-items">
-          <li>
-            <Link to="/" className="home">
-              <HomeIcon />
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="" className="works">
-              <WorkIcon />
-              Works
-            </Link>
-          </li>
-          <li>
-            <Link to="" className="skills">
-              <ComputerIcon />
-              Skills & Resume
-            </Link>
-          </li>
-        </ul>
+       <NavBar toggle={toggle} setToggle={setToggle}/>
         <div className="timeline_container">
           <HeaderTimeline />
           <VerticalTimeline>
--- src/components/AboutMe/NavBar.jsx
+++ src/components/AboutMe/NavBar.jsx
@@ -1,4 +1,44 @@
+import React from "react";
+import './NavBar.css';
+import HomeIcon from "@mui/icons-material/Home";
+import WorkIcon from "@mui/icons-material/Work";
+import ComputerIcon from "@mui/icons-material/Computer";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { Link } from "react-router-dom";
 
+function NavBar({toggle, setToggle}) {
+  const clickHandler = () => {
+    setToggle(!toggle);
+  }
+  return (
+    <>
+      <span className="menu-toggle" onClick={clickHandler}>
+        <MenuIcon className="menu-open" />
+        <CloseIcon className="menu-close" />
+      </span>
+      <ul className="menu-items">
+        <li>
+          <Link to="/" className="home">
+            <HomeIcon />
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="" className="works">
+            <WorkIcon />
+            Works
+          </Link>
+        </li>
+        <li>
+          <Link to="" className="skills">
+            <ComputerIcon />
+            Skills & Resume
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
+}
 
-
-
+export default NavBar;
